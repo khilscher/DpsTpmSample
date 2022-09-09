1272e94730e3d012a3f0ae51f419efdaf48c1a93
Kevin Hilscher <kevin@nospam>
2022-09-08 20:14:58 -0600

    Initial commit

---
+++ DpsTpmSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Devices.Provisioning.Security;
+
+namespace DpsTpmSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sample based on https://github.com/Azure-Samples/azure-iot-samples-csharp/blob/main/provisioning/Samples/device/TpmSample/Program.cs
+
+            using var security = new SecurityProviderTpmHsm(null);
+            Console.WriteLine($"Your EK is {Convert.ToBase64String(security.GetEndorsementKey())}");
+        }
+    }
+}
